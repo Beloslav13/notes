4a560b3b322c539503c1455594f1348e69cde97d
beloslav <velislav1329@gmail.com>
2020-09-29 23:02:31 +0300

    добавил права

--- notes/rest/serializers/note.py
+++ notes/rest/serializers/note.py
@@ -6,4 +6,4 @@ from notes.models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = ('id', 'name', 'owner', 'priority', 'readers',  'url', 'is_done', 'created_at')
--- notes/views/note.py
+++ notes/views/note.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from notes.models import Note
 from notes.rest.serializers.note import NoteSerializer
@@ -7,3 +8,4 @@ from notes.rest.serializers.note import NoteSerializer
 class NoteViewSet(viewsets.ModelViewSet):
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
