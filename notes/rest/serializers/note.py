367ae33489ae40603657afac7bbe58104dc86ea5
beloslav <velislav1329@gmail.com>
2020-10-03 15:43:48 +0300

    реализовал обратный механизм снятия даты поля дата закрытия заметки

--- notes/rest/serializers/note.py
+++ notes/rest/serializers/note.py
@@ -21,10 +21,15 @@ class NoteSerializer(serializers.ModelSerializer):
     @staticmethod
     def _update_entity(self, instance, validated_data):
         is_done = validated_data.pop('is_done', None)
-        if is_done is not None and is_done != instance.is_done:
-            validated_data.pop('solved_at', None)
-            instance.solved_at = timezone.now()
-            instance.is_done = is_done
+        if is_done is not None:
+            if not instance.is_done and is_done != instance.is_done:
+                validated_data.pop('solved_at', None)
+                instance.solved_at = timezone.now()
+                instance.is_done = is_done
+            elif instance.is_done and instance.solved_at and is_done != instance.is_done:
+                validated_data.pop('solved_at', None)
+                instance.solved_at = None
+                instance.is_done = is_done
             instance.save()
 
     def update(self, instance, validated_data):
