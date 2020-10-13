0eb3c2568723e2dde60eb7144ca244546a3c5621
beloslav <velislav1329@gmail.com>
2020-10-13 16:13:05 +0300

    fix

--- notes/models/note.py
+++ notes/models/note.py
@@ -12,14 +12,48 @@ class Note(models.Model):
         (5, 'Наивысший')
     )
 
-    name = models.CharField(max_length=255, blank=False, null=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='my_notes')
-    priority = models.PositiveSmallIntegerField(choices=PRIORITY_CHOICE, blank=False, null=False)
-    readers = models.ManyToManyField(User, related_name='read_notes', blank=True)
-    is_done = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    solved_at = models.DateTimeField(auto_now=False, blank=True, null=True, db_index=True)
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False
+    )
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='my_notes'
+    )
+
+    priority = models.PositiveSmallIntegerField(
+        choices=PRIORITY_CHOICE,
+        blank=False,
+        null=False
+    )
+
+    readers = models.ManyToManyField(
+        User,
+        related_name='read_notes',
+        blank=True
+    )
+
+    is_done = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    solved_at = models.DateTimeField(
+        auto_now=False,
+        blank=True,
+        null=True,
+        db_index=True
+    )
 
     class Meta:
         verbose_name = 'Заметка'
