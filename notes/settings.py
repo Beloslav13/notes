3f3e9d532ff7606a6e007a587931248864de8995
beloslav <velislav1329@gmail.com>
2020-10-13 15:54:08 +0300

    расположение шаблонов

--- notes/settings.py
+++ notes/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': os.path.join(BASE_DIR, 'templates', 'notes', 'notes'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
