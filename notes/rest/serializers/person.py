be4c23e59b1c3b55af884fe501f3470403dde4dd
beloslav <velislav1329@gmail.com>
2020-10-14 16:45:20 +0300

    добавил логгер, обработал возможное исключение

--- notes/rest/serializers/person.py
+++ notes/rest/serializers/person.py
@@ -4,6 +4,10 @@ from rest_framework import serializers
 from notes.models.note import Note
 from notes.models.person import Person
 
+import logging
+
+logging.basicConfig(filename='log.log', filemode='a', format='%(asctime)s %(message)s')
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,7 +20,8 @@ class UserSerializer(serializers.ModelSerializer):
     read_notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
     # person = PersonSerializer(read_only=True, many=False)
     phone = serializers.CharField(source='person.phone', allow_blank=True, allow_null=True)
-    birthday = serializers.CharField(source='person.birthday', allow_blank=True, allow_null=True)
+    birthday = serializers.CharField(source='person.birthday')
+    # todo: это поле возможно не нужно
     person_id = serializers.IntegerField(source='person.id', read_only=True)
     person = serializers.PrimaryKeyRelatedField(read_only=True)
 
@@ -26,11 +31,23 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'url', 'person', 'first_name', 'last_name', 'username', 'email',
                   'person_id', 'phone', 'birthday', 'my_notes', 'read_notes')
 
+    def validate(self, data):
+        attr_errors = {}
+        if not data['person']['birthday']:
+            attr_errors['birthday'] = "Это поле не было заполнено."
+            raise serializers.ValidationError(attr_errors)
+        return data
+
     def create(self, validated_data):
-        # При создании кастомного юзера, создаём расширенного.
+        # При создании кастомного юзера, создаём персону.
         persons_data = validated_data.pop('person')
         validated_data.pop('my_notes')
         validated_data.pop('read_notes')
         user = User.objects.create(**validated_data)
-        Person.objects.create(person=user, **persons_data)
+        try:
+            Person.objects.create(person=user, **persons_data)
+        except TypeError as exc:
+            person = Person.objects.create(person=user)
+            logging.error(f'Произошла ошибка при создании персоны в сериалайзере: {exc}\n'
+                          f'user id {user.id}, person id {person.id}')
         return user
