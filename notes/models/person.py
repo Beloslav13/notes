dcb24002be39dbcb569dff3cd047f96f8378c88f
beloslav <velislav1329@gmail.com>
2020-10-14 12:46:47 +0300

    сделал сеттеры

--- notes/models/person.py
+++ notes/models/person.py
@@ -22,5 +22,43 @@ class Person(models.Model):
         default=None
     )
 
+    # todo: last_activity
+
     def __str__(self):
         return f'{self.person.username} id:{self.id}'
+
+    @property
+    def username(self):
+        return self.person.username
+
+    @username.setter
+    def username(self, value):
+        self.person.username = value
+
+    @property
+    def first_name(self):
+        return self.person.first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self.person.first_name = value
+
+    @property
+    def last_name(self):
+        return self.person.last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self.person.last_name = value
+
+    @property
+    def email(self):
+        return self.person.email
+
+    @email.setter
+    def email(self, value):
+        self.person.email = value
+
+    @property
+    def get_person(self):
+        return self.person
