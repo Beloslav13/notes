b597ee27507fc2fc6bdffd2995d65c442bc55816
beloslav <velislav1329@gmail.com>
2020-10-26 21:15:09 +0300

    fix

--- notes/rest/serializers/person.py
+++ notes/rest/serializers/person.py
@@ -18,16 +18,12 @@ class PersonSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     my_notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
     read_notes = serializers.PrimaryKeyRelatedField(many=True, queryset=Note.objects.all())
-    # person = PersonSerializer(read_only=True, many=False)
     phone = serializers.CharField(source='person.phone', allow_blank=True, allow_null=True)
     birthday = serializers.CharField(source='person.birthday')
-    # todo: это поле возможно не нужно
-    # person_id = serializers.IntegerField(source='person.id', read_only=True)
     person = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = User
-        # depth = 1
         fields = ('id', 'url', 'person', 'first_name', 'last_name', 'username', 'email',
                   'phone', 'birthday', 'my_notes', 'read_notes')
 
