d5288af1917ef5f5e7df87b39efd5bbd5590035e
beloslav <velislav1329@gmail.com>
2020-11-02 16:12:19 +0300

    добавил поля по количеству заметок и читаемых заметок

--- notes/views/person.py
+++ notes/views/person.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
@@ -12,3 +13,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     filter_backends = [DjangoFilterBackend, SearchFilter]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        """Получаем базовый queryset и добавляем annotate."""
+        queryset = super(UserViewSet, self).get_queryset()
+        queryset = queryset.annotate(
+            count_notes=Count('my_notes', distinct=True),
+            count_read_notes=Count('read_notes', distinct=True)
+        )
+        return queryset
