9bbdcb179a8d7d9a14d8a3a00cc7aee6a8ffe9dd
beloslav <velislav1329@gmail.com>
2020-11-05 19:08:35 +0300

    реализовал последнюю активность персоны при смене состояния

--- notes/signals/handlers/note.py
+++ notes/signals/handlers/note.py
@@ -1,5 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+
+from django_fsm.signals import post_transition
 
 from notes.models.note import Note
 
@@ -8,3 +11,10 @@ from notes.models.note import Note
 @receiver(post_save, sender=Note, dispatch_uid='invalidate_note')
 def invalidate_note(sender, instance, **kwargs):
     pass
+
+
+@receiver(post_transition, sender=Note, dispatch_uid='invalidate_note_transition')
+def invalidate_note_transition(sender, instance, name, source, target, **kwargs):
+    if name:
+        instance.owner.person.last_activity = timezone.now()
+        instance.owner.person.save()
